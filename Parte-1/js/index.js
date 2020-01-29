c7d22690862014587b7472d0bf0c97ef52bd4437
CyberFroppy <CyberFroppy@users.noreply.github.com>
2020-01-29 07:32:44 -0600

    Iniciando Examen

---
+++ Parte-1/js/index.js
@@ -0,0 +1,17 @@
+let url = "https://restcountries.eu/"
+/*Nombre del país
+ii. Capital
+iii. Imagen de su bandera
+iv. Población
+v. Región
+vi. Zonas horarias
+vii. Países con los que colinda
+*/
+function watchForm(){
+    
+}
+
+function init(){
+    watchForm();
+}
+init();
